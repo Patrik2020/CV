2cd22930a9e745431cbdc5363042c4675e420a3f
Kovács Patrik <65179610+Patrik2020@users.noreply.github.com>
2024-04-29 21:37:21 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
-let a = "Bruttó: ";
-let b = 300;
+let a = "Nettó: ";
+let b = 400;
 let c = " 000";
 let d = " -";
-let e =" 600";
+let e =" 500";
 let f =" 000";
 let g=" Ft"
 let result = a + b + c + d + e + f + g;
