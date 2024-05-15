9ee290a8e9733c22f555db23a766258bc28b3405
Kovács Patrik <65179610+Patrik2020@users.noreply.github.com>
2024-05-15 22:16:23 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,6 @@
-let a = "Nettó: ";
-let b = 400;
-let c = " 000";
-let d = " -";
-let e =" 500";
-let f =" 000";
-let g=" Ft"
-let result = a + b + c + d + e + f + g;
+function yesAnswer() {
+    document.getElementById("yes").innerHTML = "Br.: 610 000 Ft";
+  }
+  function noAnswer() {
+    document.getElementById("no").innerHTML = "Dehogynem... :)";
+  }
